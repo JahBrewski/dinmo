1610f24c1bab4e4032af5009108bba6a1aa9d3fb
Joel Brewer <joelbrewer01@gmail.com>
2014-05-23 13:25:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dinmo::Application.config.secret_key_base = '4ed9ef80703f4fb06429ce5d4a32ec348410472835391fa7e0927ff9d019976c2a5031d22f213de085ed7a6b7919fb7e4d242e546e30a11b01df5bec9929e88e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dinmo::Application.config.session_store :cookie_store, key: '_dinmo_session'
