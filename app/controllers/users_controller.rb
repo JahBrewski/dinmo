69117802779b9b701acc84ee0adb70542901e33d
Joel Brewer <joelbrewer01@gmail.com>
2014-07-02 12:44:31 -0500

    add jon to every search result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
+  JON_MCEWEN = User.find_by username: "jonmcewen"
   def show
   end
 
   def index
     if params[:search]
-      @users = User.search(params[:search])
+      @users = User.search(params[:search]) | [JON_MCEWEN]
     else
-      @users = []
+      @users = [JON_MCEWEN]
     end
   end
 end
